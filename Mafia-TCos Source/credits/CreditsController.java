987cf4486ea6e5c8a44620b9e9976638a619afe4
melisocal <basakmelisocal@hotmail.com>
2017-05-02 14:35:53 +0300

    Add files via upload

---
+++ Mafia-TCos Source/credits/CreditsController.java
@@ -0,0 +1,16 @@
+package sample;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class CreditsController {
+
+    public Button backButton;
+
+    public void buttonPressed(MouseEvent actionEvent)
+    {
+        System.out.println("Button pressed!");
+    }
+
+
+}
