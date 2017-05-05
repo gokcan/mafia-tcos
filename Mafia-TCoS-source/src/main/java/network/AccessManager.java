1314982ea15cc3a224ec0944db7b9eb7bee8296b
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-05-05 23:38:19 +0300

    AccessManager updated

--- Mafia-TCoS-source/src/main/java/network/AccessManager.java
+++ Mafia-TCoS-source/src/main/java/network/AccessManager.java
@@ -1,14 +1,20 @@
 package network;
 /**
- * @Author: Skylifee7 on 01/05/2017.
+ * Author: Skylifee7 on 01/05/2017.
+ * ----- ----- ----- ----- ----- ----- -----  * ----- ----- ----- ----- ----- ----- -----
+ * Access Manager class handles almost all the communications between Client and Server(REST)
+ * via usual HTTP(S) Requests (GET/POST/PUT/DELETE). OnSuccess and OnFailure listeners are really helpful
+ * to achieve low coupling. AccessManager class never talks with the UI components. Yeah, she is a little shy..
  */
 
+import Model.Crime;
 import Model.Player;
-import retrofit2.*;
 import retrofit2.Call;
+import retrofit2.*;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.prefs.Preferences;
+import java.io.IOException;
 
 public class AccessManager {
 
@@ -19,6 +25,22 @@ public class AccessManager {
     private String user_id = "User-ID";
     private SharedDataHelper sharedDataHelper;
     private AccessClientListener listener;
+
+    public interface AccessClientListener {
+        void onLoginFailure(String response);
+
+        void onSignupFailure(String response);
+
+        void onFetchPlayerSuccess(Player player);
+
+        void onFetchPlayerFailure(String response);
+
+        void onLoginSuccess(String response);
+
+        void onSignupSuccess(String response);
+
+        void onFetchCrimesSuccess(ArrayList<Crime> crimes);
+    }
 
     public AccessManager() {
         // set null or default listener or accept as argument to constructor
@@ -47,29 +69,26 @@ public class AccessManager {
                     auth_token = response.body().authenticationToken;
                     user_id = response.body().playerUID;
 
+                    //String emoji = new String(Character.toChars(0xF160E));
                     sharedDataHelper.putUserID(user_id);
                     sharedDataHelper.putUserToken(auth_token);
 
-                    if(listener!=null)
-                    listener.onLoginSuccess("Success");
-
-                    System.out.println("Success.");
+                    if (listener != null)
+                        listener.onLoginSuccess("Welcome back " + userName + " :)");
 
                 } else if (response.code() == 401 || response.code() == 403)
-                    System.out.println("Unauthorized access.");
+                    listener.onLoginFailure("Unauthorized access.");
                 else
-                    System.out.println("Unsuccessful.");
-
-
+                    listener.onLoginFailure("Server is unavailable.");
             }
 
             @Override
             public void onFailure(Call<Authentication> call, Throwable t) {
 
                 if (t instanceof IOException)
-                    System.out.println("Check your network connection.");
+                    listener.onLoginFailure("Check your internet connection.");
                 else
-                    System.out.println("Service is unavailable.");
+                    listener.onLoginFailure("Service is unavailable.");
             }
         });
     }
@@ -94,30 +113,30 @@ public class AccessManager {
                     user_id = response.body().playerUID;
 
                     sharedDataHelper.putUserID(user_id);
-                    System.out.println("Success.");
+
+                    if (listener != null)
+                        listener.onSignupSuccess("Please login now " + userName + " :)");
 
 
                 } else if (response.code() == 401 || response.code() == 403)
-                    System.out.println("Unauthorized access.");
+                    listener.onSignupFailure("Unauthorized access.. Check your credentials.");
                 else
-                    System.out.println("Unsuccessful.");
-
+                    listener.onSignupFailure("Server is unavailable.");
             }
 
             @Override
             public void onFailure(Call<Authentication> call, Throwable t) {
 
                 if (t instanceof IOException)
-                    System.out.println("Check your network connection.");
+                    listener.onSignupFailure("Check your internet connection.");
                 else
-                    System.out.println("Service is unavailable.");
+                    listener.onSignupFailure("Service is unavailable.");
             }
         });
 
-
     }
 
-    protected void callGetPlayerData() {
+    public void callGetPlayerData() {
 
         sharedDataHelper = new SharedDataHelper();
 
@@ -129,10 +148,45 @@ public class AccessManager {
             public void onResponse(Call<Player> call, Response<Player> response) {
 
                 if (response != null && response.body() != null) {
+
                     final Player player1;
                     player1 = response.body();
 
+                    if (listener != null)
+                        listener.onFetchPlayerSuccess(player1);
+
                     System.out.println(player1.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Player> call, Throwable t) {
+
+                if (t instanceof IOException)
+                    listener.onFetchPlayerFailure("Check your internet connection.");
+                else
+                    listener.onFetchPlayerFailure("Service is unavailable.");
+            }
+        });
+    }
+
+    public void callUpdateService(Player player) {
+
+        APIClient apiClient = APICallHelper.getInstance().create(APIClient.class);
+
+        Call<Player> update = apiClient.updatePlayerData(sharedDataHelper.getUserToken(), sharedDataHelper.getUserID(), player);
+        update.enqueue(new Callback<Player>() {
+
+            @Override
+            public void onResponse(Call<Player> call, Response<Player> response) {
+
+                if (response != null && response.body() != null && response.isSuccessful()) {
+
+                    System.out.println("Success. Player has been updated.");
+
+                } else if (response.code() == 401 || response.code() == 403) {
+
+                    System.out.println("Unauthorized access.");
                 }
             }
 
@@ -145,6 +199,36 @@ public class AccessManager {
                     System.out.println("Service is unavailable.");
             }
         });
+
+    }
+
+    public void callGetCrimes() {
+
+        APIClient apiClient = APICallHelper.getInstance().create(APIClient.class);
+        Call <ArrayList<Crime>> getCrimes = apiClient.getCrimes();
+        getCrimes.enqueue(new Callback<ArrayList<Crime>>() {
+
+            @Override
+            public void onResponse(Call<ArrayList<Crime>> call, Response<ArrayList<Crime>> response) {
+
+                if (response != null && response.body() != null) {
+
+                    ArrayList<Crime> crimeArrayList = response.body();
+
+                    if (listener != null)
+                        listener.onFetchCrimesSuccess(crimeArrayList);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Crime>> call, Throwable t) {
+
+                if (t instanceof IOException)
+                    listener.onFetchPlayerFailure("Check your internet connection.");
+                else
+                    listener.onFetchPlayerFailure("Service is unavailable.");
+            }
+        });
     }
 
     private LoginInformation getBodyParameter() {
@@ -153,13 +237,6 @@ public class AccessManager {
         loginInformation.setUsername(userName);
         loginInformation.setPassword(userPassword);
         return loginInformation;
-    }
-
-    public interface AccessClientListener {
-        void onLoginFailure(ErrorType type);
-        void onSignupFailure(ErrorType type);
-        void onLoginSuccess(String response);
-        void onSignupSuccess(Player player);
     }
 
 
