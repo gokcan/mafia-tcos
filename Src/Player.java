bae910df66d921711238fc96ea716b1a71f694e2
gokberkaktulay <gokberkaktulay@gmail.com>
2017-05-06 14:35:12 +0300

    Update Player.java

--- Src/Player.java
+++ Src/Player.java
@@ -1,22 +1,16 @@
 import java.util.*;
 
 /*
-* AchievementTracker needs some of the methods here properly function
+* AchievementTracker needs some of the methods and properties here to function properly
 */
 
 public class Player{
    private ArrayList<Achievement> locked;
    private ArrayList<Achievement> unlocked;
-   public double drug;
-   public double money;
-   public double launderedMoney;
    
    public Player(){
       locked = new ArrayList<Achievement>();
       unlocked = new ArrayList<Achievement>();
-      drug = 0;
-      money = 0;
-      launderedMoney = 0;
    }
    
    // creates and add an achievement in the locked list
@@ -29,7 +23,7 @@ public class Player{
       return locked.size();
    }
    
-   // seachers for the achievement in the locked list, removes it and adds it to unlocked
+   // search for the achievement in the locked list, removes it and adds it to unlocked
    public void unlockAchievement(int ID){
       Achievement a = null;
       for (int i = 0; i < locked.size() && a == null;i++){
@@ -41,31 +35,9 @@ public class Player{
       }
    }
    
-   public double getDrug(){
-      return drug;
-   }
-   
-   public double getMoney(){
-      return money;
-   }
-      
-   public double getLaunderedMoney(){
-      return launderedMoney;
-   }
-   
    // returns locked achievement in the given index
    public Achievement getAchievement(int i){
       return locked.get(i);
-   }
-   
-   // not normally used, for testing purposes
-   public void printAchis(){
-      System.out.println("Locked:");
-      for (int i = 0; i < locked.size();i++)
-         System.out.println(locked.get(i).getDescription());
-      System.out.println("Unlocked:");
-      for (int i = 0; i < unlocked.size();i++)
-         System.out.println(unlocked.get(i).getDescription());
    }
    
    // returns the IDs of unlocked achievements so they can be saved
