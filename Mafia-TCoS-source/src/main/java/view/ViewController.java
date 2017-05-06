ae340a294c05a223779468c5fe0ab02275aeb296
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-05-06 22:30:09 +0300

    Initial ViewController

--- Mafia-TCoS-source/src/main/java/view/ViewController.java
+++ Mafia-TCoS-source/src/main/java/view/ViewController.java
@@ -1,16 +1,18 @@
 package view;
 
+import Model.Crime;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
+import javax.swing.text.View;
 import java.io.IOException;
 
 /**
- * @Author: Skylifee7 on 02/05/2017.
+ * Created by SKYLIFE on 04/05/2017.
  */
-
 public class ViewController {
 
     public ViewController() {
@@ -20,19 +22,15 @@ public class ViewController {
     public void changeView() {
 
         Stage stage;
+        ToggleGroup toggleGroup = new ToggleGroup();
+
         Parent root = null;
 
         try {
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("CrimeScene.fxml"));
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("mapSample.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        String image = getClass().getClassLoader().getResource("mafia-tcos-wallpaper.jpg").toExternalForm();
-        root.setStyle("-fx-background-image: url('" + image + "'); " +
-                "-fx-background-position: center center; " +
-                "-fx-background-size: cover"
-        );
 
         stage = Main.getPrimaryStage();
         stage.setScene(new Scene(root));
